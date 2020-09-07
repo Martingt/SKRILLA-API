00b86a119585c23dbc67f02f63cc39332302e754
Martín García Tejeda <m.garcia.tejeda@gmail.com>
2020-09-07 15:34:54 -0300

    Proyecto OAuth agregado

---
+++ Skrilla/Skrilla.OAuth/Configuration/InMemoryConfiguration.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using IdentityServer4.Models;
+using IdentityServer4.Test;
+
+namespace Skrilla.OAuth.Configuration
+{
+    public class InMemoryConfiguration
+    {
+        public static IEnumerable<ApiResource> ApiResources()
+        {
+            return new[] {
+                    new ApiResource("skrilla", "Skrilla")
+                };
+        }
+
+        public static IEnumerable<Client> Clients()
+        {
+            return new[] {
+                    new Client
+                    {
+                        ClientId = "skrilla",
+                        ClientSecrets = new [] { new Secret("secret".Sha256()) },
+                        AllowedGrantTypes = GrantTypes.ResourceOwnerPasswordAndClientCredentials,
+                        AllowedScopes = new [] { "skrilla" }
+                    }
+                };
+        }
+
+        public static IEnumerable<TestUser> Users()
+        {
+            return new[] {
+                new TestUser
+                {
+                    SubjectId = "1",
+                    Username = "m.garcia.tejeda@gmail.com",
+                    Password = "password"
+
+                }
+            };
+
+        }
+    }
+}
---
+++ Skrilla/Skrilla.OAuth/Startup.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Skrilla.OAuth.Configuration;
+
+namespace Skrilla.OAuth
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddIdentityServer()
+                .AddDeveloperSigningCredential()
+                .AddTestUsers(InMemoryConfiguration.Users().ToList())
+                .AddInMemoryClients(InMemoryConfiguration.Clients())
+                .AddInMemoryApiResources(InMemoryConfiguration.ApiResources());
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseIdentityServer();
+        }
+    }
+}
