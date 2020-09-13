dc060e04f1cb2f1d67b911c16f7b287b152b2b35
Martín García Tejeda <m.garcia.tejeda@gmail.com>
2020-09-13 20:54:03 -0300

    Agregando SQL server

--- Skrilla/Skrilla.OAuth/Startup.cs
+++ Skrilla/Skrilla.OAuth/Startup.cs
@@ -1,12 +1,16 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
+using IdentityServer4.EntityFramework.DbContexts;
+using IdentityServer4.EntityFramework.Mappers;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -26,13 +30,32 @@ namespace Skrilla.OAuth
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            const string connectionString = @"Data Source=(LocalDb)\MSSQLLocalDB;database=IdentityServer4.Quickstart.EntityFramework-2.0.0;trusted_connection=yes;";
+            var migrationsAssembly = typeof(Startup).GetTypeInfo().Assembly.GetName().Name;
             services.AddIdentityServer()
                 .AddSigningCredential(new X509Certificate2(@"skrilla.pfx", "alagrandelepusecuca"))
                 .AddTestUsers(InMemoryConfiguration.Users().ToList())
-                .AddInMemoryClients(InMemoryConfiguration.Clients())
-                .AddInMemoryIdentityResources(InMemoryConfiguration.IdentityResources())
-                .AddInMemoryApiResources(InMemoryConfiguration.ApiResources())
-                .AddInMemoryApiScopes(InMemoryConfiguration.ApiScopes());
+                .AddConfigurationStore(options =>
+                {
+                    options.ConfigureDbContext = builder =>
+                        builder.UseSqlServer(connectionString,
+                            sql => sql.MigrationsAssembly(migrationsAssembly));
+                })
+                // this adds the operational data from DB (codes, tokens, consents)
+                .AddOperationalStore(options =>
+                {
+                    options.ConfigureDbContext = builder =>
+                        builder.UseSqlServer(connectionString,
+                            sql => sql.MigrationsAssembly(migrationsAssembly));
+
+                    // this enables automatic token cleanup. this is optional.
+                    options.EnableTokenCleanup = true;
+                    options.TokenCleanupInterval = 30;
+                });
+                //.AddInMemoryClients(InMemoryConfiguration.Clients())
+                //.AddInMemoryIdentityResources(InMemoryConfiguration.IdentityResources())
+                //.AddInMemoryApiResources(InMemoryConfiguration.ApiResources())
+                //.AddInMemoryApiScopes(InMemoryConfiguration.ApiScopes());
 
             services.AddMvc(option => option.EnableEndpointRouting = false);
         }
@@ -40,10 +63,49 @@ namespace Skrilla.OAuth
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            InitializeDatabase(app);
+
             app.UseDeveloperExceptionPage();
             app.UseIdentityServer();
             app.UseStaticFiles();
             app.UseMvcWithDefaultRoute();
+        }
+
+        private void InitializeDatabase(IApplicationBuilder app)
+        {
+            using (var serviceScope = app.ApplicationServices.GetService<IServiceScopeFactory>().CreateScope())
+            {
+                serviceScope.ServiceProvider.GetRequiredService<PersistedGrantDbContext>().Database.Migrate();
+
+                var context = serviceScope.ServiceProvider.GetRequiredService<ConfigurationDbContext>();
+                context.Database.Migrate();
+                if (!context.Clients.Any())
+                {
+                    foreach (var client in InMemoryConfiguration.Clients())
+                    {
+                        context.Clients.Add(client.ToEntity());
+                    }
+                    context.SaveChanges();
+                }
+
+                if (!context.IdentityResources.Any())
+                {
+                    foreach (var resource in InMemoryConfiguration.IdentityResources())
+                    {
+                        context.IdentityResources.Add(resource.ToEntity());
+                    }
+                    context.SaveChanges();
+                }
+
+                if (!context.ApiResources.Any())
+                {
+                    foreach (var resource in InMemoryConfiguration.ApiResources())
+                    {
+                        context.ApiResources.Add(resource.ToEntity());
+                    }
+                    context.SaveChanges();
+                }
+            }
         }
     }
 }
