453eda0e606a026607e650bfa06fc2b5295bee37
Martín García Tejeda <m.garcia.tejeda@gmail.com>
2020-09-14 20:30:48 -0300

    Actualizacion

--- Skrilla/Skrilla.OAuth/Startup.cs
+++ Skrilla/Skrilla.OAuth/Startup.cs
@@ -9,6 +9,7 @@ using IdentityServer4.EntityFramework.Mappers;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
@@ -32,6 +33,24 @@ namespace Skrilla.OAuth
         {
             const string connectionString = @"Data Source=(LocalDb)\MSSQLLocalDB;database=IdentityServer4.Quickstart.EntityFramework-2.0.0;trusted_connection=yes;";
             var migrationsAssembly = typeof(Startup).GetTypeInfo().Assembly.GetName().Name;
+
+            //services.AddIdentity<IdentityUser, IdentityRole>(config =>
+            //{
+            //    config.Password.RequiredLength = 4;
+            //    config.Password.RequireDigit = false;
+            //    config.Password.RequireNonAlphanumeric = false;
+            //    config.Password.RequireUppercase = false;
+            //})
+            //    .AddEntityFrameworkStores
+
+            //services.ConfigureApplicationCookie(config =>
+            //{
+            //    config.Cookie.Name = "IdentityServer.Cookie";
+            //    config.LoginPath = "/Auth/Login";
+            //});
+
+
+                
             services.AddIdentityServer()
                 .AddSigningCredential(new X509Certificate2(@"skrilla.pfx", "alagrandelepusecuca"))
                 .AddTestUsers(InMemoryConfiguration.Users().ToList())
@@ -42,20 +61,20 @@ namespace Skrilla.OAuth
                             sql => sql.MigrationsAssembly(migrationsAssembly));
                 })
                 // this adds the operational data from DB (codes, tokens, consents)
-                .AddOperationalStore(options =>
-                {
-                    options.ConfigureDbContext = builder =>
-                        builder.UseSqlServer(connectionString,
-                            sql => sql.MigrationsAssembly(migrationsAssembly));
+                //.AddOperationalStore(options =>
+                //{
+                //    options.ConfigureDbContext = builder =>
+                //        builder.UseSqlServer(connectionString,
+                //            sql => sql.MigrationsAssembly(migrationsAssembly));
 
-                    // this enables automatic token cleanup. this is optional.
-                    options.EnableTokenCleanup = true;
-                    options.TokenCleanupInterval = 30;
-                });
-                //.AddInMemoryClients(InMemoryConfiguration.Clients())
-                //.AddInMemoryIdentityResources(InMemoryConfiguration.IdentityResources())
-                //.AddInMemoryApiResources(InMemoryConfiguration.ApiResources())
-                //.AddInMemoryApiScopes(InMemoryConfiguration.ApiScopes());
+                //    // this enables automatic token cleanup. this is optional.
+                //    options.EnableTokenCleanup = true;
+                //    options.TokenCleanupInterval = 30;
+                //});
+                .AddInMemoryClients(InMemoryConfiguration.Clients())
+                .AddInMemoryIdentityResources(InMemoryConfiguration.IdentityResources())
+                .AddInMemoryApiResources(InMemoryConfiguration.ApiResources())
+                .AddInMemoryApiScopes(InMemoryConfiguration.ApiScopes());
 
             services.AddMvc(option => option.EnableEndpointRouting = false);
         }
@@ -63,7 +82,7 @@ namespace Skrilla.OAuth
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            InitializeDatabase(app);
+            //InitializeDatabase(app);
 
             app.UseDeveloperExceptionPage();
             app.UseIdentityServer();
