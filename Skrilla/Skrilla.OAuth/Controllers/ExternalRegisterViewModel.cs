8e690f34f5f48a41d28d8f1568380b3d9bcd4b4f
Martín García Tejeda <m.garcia.tejeda@gmail.com>
2020-09-23 19:51:50 -0300

    AppDbContext agregado

---
+++ Skrilla/Skrilla.OAuth/Controllers/ExternalRegisterViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Skrilla.OAuth.Controllers
+{
+    public class ExternalRegisterViewModel
+    {
+        public string Username { get; set; }
+        public string ReturnUrl { get; set; }
+    }
+}
