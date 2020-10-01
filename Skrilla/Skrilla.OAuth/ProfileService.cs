4ad00b27a4ab6d987d8f10f7da7bc8ce218f8800
Martin Pordomingo <martinpordomingo@gmail.com>
2020-10-01 13:34:44 -0300

    ProfileService.cs: ProfileService added. User id now included in   token claims.

---
+++ Skrilla/Skrilla.OAuth/ProfileService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using IdentityServer4.Models;
+using IdentityServer4.Services;
+using Microsoft.AspNetCore.Identity;
+
+namespace Skrilla.OAuth
+{
+    public class ProfileService : IProfileService 
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public ProfileService(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public Task GetProfileDataAsync(ProfileDataRequestContext context)
+        {
+            List<Claim> claims = new List<Claim>() {
+                new Claim("userId", _userManager.GetUserId(context.Subject)) };
+
+            context.IssuedClaims = claims;
+
+
+            return Task.FromResult(0);
+        }
+
+        public Task IsActiveAsync(IsActiveContext context)
+        {
+            return Task.FromResult(0);
+        }
+    }
+
+    
+
+
+}
